503c79676122b54982b9a4944d4fd4d4afac693f
akaldhaheri <akaldhaheri@mail.csuchico.edu>
2018-05-01 20:16:38 -0700

    Student Manager

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Abdulla Aldhaheri
 #include <iostream>
 #include <string>
 #include "student.h"
@@ -6,16 +7,18 @@ using namespace std;
 
 int main()
 {
-    string firstName;
-    string lastName;
-    int number;
-    student person;
-    
-    cout<<"What is the student's name? ";
-    cin>>firstName>>lastName;
-    
-    person.setName(firstName,lastName);
-    cout<<"Saved "<<person.fullName()<<endl;
-    
-    return 0;
+        string firstName;
+        string lastName;
+        int number;
+        student person;
+
+        cout<<"What is the student's name? ";
+        cin>>firstName>>lastName;
+
+        person.setName(firstName,lastName);
+        cout<<"Saved "<<person.fullName()<<endl;
+
+        return 0;
 }
+
+
